3f8329a4479a5cdfdf37b882ff219af9c9aefffb
LastnAccount <llorenz.10022002@gmail.com>
2024-05-19 17:03:26 +0800

    Initial commit

---
+++ utils/db.js
@@ -0,0 +1,14 @@
+// utils/db.js
+import mysql from 'mysql2/promise';
+
+export async function query(sql, values) {
+  const db = await mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'renz',
+    database: 'webfin',
+  });
+  const [results] = await db.execute(sql, values);
+  db.end();
+  return results;
+}
